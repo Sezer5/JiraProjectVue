b8bbb2783bf9575cdee83dd770f3c290c345e6b4
Sezer5 <unalmissezer@gmail.com>
2025-11-09 23:11:16 +0300

    Taskların Css Düzenlemeleri

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  background-color:ghostwhite;
+}
+#app{
+     font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      width: 100%;
+      max-width: 600px;
+      margin: 0px auto;
 }
 
 
