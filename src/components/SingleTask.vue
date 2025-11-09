42f37bdd828b876bcbc52229f034207f4ef6f7da
Sezer5 <unalmissezer@gmail.com>
2025-11-09 22:19:04 +0300

    Toggle İle Açma Kapama Özelliği Eklenmesi

---
+++ src/components/SingleTask.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="task">
+        <div class="actions">
+            <h3 @click="toggleDetail()">{{task.title}}</h3>
+        </div>
+        <div class="details" v-if="showDetails">
+            <p>{{task.details}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['task'],
+    data(){
+        return{
+            showDetails:false
+        }
+    },
+    methods:{
+        toggleDetail(){
+            this.showDetails=!this.showDetails;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,8 @@
   <div class="home">
       <div v-if="tasks.length">
             <div v-for="task in tasks" :key="task.id">
-                  <p>{{task.title}}</p>
+                  <!-- <p>{{task.title}}</p> -->
+                  <SingleTask :task="task"/>
             </div>
       </div>
   </div>
@@ -10,10 +11,11 @@
 
 <script>
 // @ is an alias to /src
-
+import SingleTask from '../components/SingleTask.vue'
 export default {
   name: 'HomeView',
   components: {
+      SingleTask
   },
   data(){
     return{
