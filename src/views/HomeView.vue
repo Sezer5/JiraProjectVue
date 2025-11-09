d2240d49e9ab28c67ecc277e507352e4a3351467
Sezer5 <unalmissezer@gmail.com>
2025-11-09 19:46:19 +0300

    npm server ile json data oluşturma ve çekme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+      <div v-if="tasks.length">
+            <div v-for="task in tasks" :key="task.id">
+                  <p>{{task.title}}</p>
+            </div>
+      </div>
   </div>
 </template>
 
@@ -9,6 +14,14 @@
 export default {
   name: 'HomeView',
   components: {
+  },
+  data(){
+    return{
+        tasks:[]
+    }
+  },
+  mounted(){
+      fetch('http://localhost:3000/tasks').then(res=>res.json()).then(data=>this.tasks=data).catch(err=>console.log(err.message));
   }
 }
 </script>
