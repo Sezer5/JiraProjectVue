d46f9a488ca7ae646f2d31b3f9cc9b4b300e018e
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-11 11:43:48 +0300

    Ekleme Kısmı Formu Eklendi

---
+++ src/views/AddTask.vue
@@ -0,0 +1,24 @@
+<template>
+    <form>
+            <label>Başlık</label>
+            <input type="text" v-model="title" required>
+            <label>İş Detayı</label>
+            <textarea v-model="details" required></textarea>
+            <button>Ekle</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'',
+            details:''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
