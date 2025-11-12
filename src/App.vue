9f9b0ba733de82682f685909b1a816818de37a0e
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-12 13:19:07 +0300

    Navbar Cssleri

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="app">
-    <nav>
-      <router-link to="/">Home</router-link>
+    <nav class="mainNav">
+      <router-link to="/">Yapılacaklar Listesi</router-link>  
+      <router-link to="/addTask">Yeni Görev Ekle</router-link>
     </nav>
     <router-view/>
   </div>
@@ -24,6 +25,19 @@ body{
       max-width: 600px;
       margin: 0px auto;
 }
+.mainNav{
+  text-align: center;
+  margin:40px 0px;
+}
 
-
+.mainNav a{
+  text-decoration: none;
+  color:gray;
+  font-size: 18px;
+  margin: 0px 10px;
+}
+a.router-link-active{
+    border-bottom:3px solid green;
+    padding-bottom: 4px;
+}
 </style>
